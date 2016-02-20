dc109cfdc70be11f194bd07e47fd32665505d86e
jimmijazz <joshua.bitossi@gmail.com>
2016-02-21 00:18:05 +1000

    MySQL first commit

---
+++ Database_connect.py
@@ -0,0 +1,31 @@
+#Setting up local MySQL Server#
+#Start MySQL in System Preferences
+#Run Mamp -> Start Servers
+#Ensure correct port is being used.
+
+#Set Database Configs
+user="root"
+password="password"
+host="localhost"
+port="8889"
+database="mysql"
+
+
+import mysql.connector
+from mysql.connector import Error
+
+#connect to database
+def connect():
+    try:
+        conn = mysql.connector.connect(user=user,password=password,host=host,port=port,database=database)
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    connect()
